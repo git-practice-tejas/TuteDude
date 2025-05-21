1db99a8f0641a9258d49b2dc52d768132645ff86
git-practice-tejas <tejaschaudhari9960@gmail.com>
2025-05-21 13:01:44 +0530

    Assignment_1

---
+++ Assignment01.py
@@ -0,0 +1,30 @@
+# Task1
+'''Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+ o	        Addition
+ o	        Subtraction
+ o	        Multiplication
+ o	        Division
+ 3.  Displays the results of each operation on the screen.'''
+
+# op_1 = int(input("Enter the first number: "))
+# op_2 = int(input("Enter the second number: "))
+# # operator = input("Enter the operatopr (+,-,*,/)")
+#
+# print(f"Addition: {op_1 + op_2}")
+# print(f"Subtraction: {op_1 - op_2}")
+# print(f"Multiplication: {op_1 * op_2}")
+# print(f"Division: {op_1 / op_2}")
+
+#Task2
+'''Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: " )
+
+print(f"Hello, {f_name} {l_name}! Welcome to the Python program")
--- first.py
+++ first.py
@@ -1,1 +1,35 @@
-print("This is my first py file")
+#print("This is my first py file")
+
+# a = int(input("Enter a number: "))
+#
+# if a > 5:
+#     print("a is  biger than the number")
+# else:
+#     print("A is small")
+
+# operand_1 = int(input("Enter the number: "))
+# operand_2 = int(input("Enter the number: "))
+#
+# operator = input("Enter the operator ('+', '-') :")
+#
+# if operator == '+':
+#     print(f"{operand_1} + {operand_2} = {operand_1+operand_2}")
+# elif operator == '-':
+#     print(f"{operand_1} + {operand_2} = {operand_1 + operand_2}")
+# else:
+#     print("Unknown operator")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
